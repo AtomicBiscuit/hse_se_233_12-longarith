7b87bc5b8efe8c7405672dd8b5e4cdf76758f23f
Idar Sokurov <idar.sokurov.05@mail.ru>
2024-01-25 19:11:30 +0300

    update header

--- lib/longarithmetic.hpp
+++ lib/longarithmetic.hpp
@@ -1,5 +1,24 @@
 #pragma once
 
+#include <vector>
+#include <cstdint>
+
+using namespace std;
+
+using digit_t = unsigned char;
+
 class BigInt {
     BigInt();
+
+    BigInt(const vector<digit_t>&, const uint32_t, const uint8_t);
+
+    BigInt(const BigInt&);
+
+    const uint32_t size() const;
+
+    const uint8_t sign() const;
+
+    const vector<digit_t>& as_array() const;
+
+    friend BigInt operator+(const BigInt&, const BigInt&);
 };
